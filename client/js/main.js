2f5b3de738d9bf64b334e652a6fb390ce1afa8e3
communitystarfoundry <community@thestarfoundry.com>
2016-08-03 05:03:21 +0000

    Add base blacktie template

--- client/js/main.js
+++ client/js/main.js
@@ -1,7 +1,7 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
-import './main.html';
+import './../views/main.html';
 
 Template.hello.onCreated(function helloOnCreated() {
   // counter starts at 0
