2e388749a3fd07e61887cd256bc0c1ebd51bd1f9
communitystarfoundry <community@thestarfoundry.com>
2016-08-11 07:47:36 +0000

    Upload candidates

--- client/js/fileUpload.js
+++ client/js/fileUpload.js
@@ -9,11 +9,51 @@ Template.sideBar.helpers({
 
     if( currentLoggedInUser ){
       if( currentLoggedInUser.emails) {
-        currentLoggedInUserName = currentLoggedInUser.emails[0].address;      
+        currentLoggedInUserName = currentLoggedInUser.emails[0].address;
       }
     }
 
     return currentLoggedInUserName;
   },
+});
+
+
+
+Template.upload.onCreated( () => {
+  Template.instance().uploading = new ReactiveVar( false );
+});
+
+Template.upload.helpers({
+  uploading() {
+    return Template.instance().uploading.get();
+  }
+});
+
+Template.upload.events({
+  'change [name="uploadCSV"]' ( event, template ) {
+
+    Papa.parse( event.target.files[0], {
+      header: true,
+      complete( results, file ) {
+        Meteor.call( 'parseUpload', results.data, ( error, response ) => {
+          if ( error ) {
+            console.log( error.reason );
+          } else {
+            template.uploading.set( false );
+            //Bert.alert( 'Upload complete!', 'success', 'growl-top-right' );
+            console.log("upload succeeded");
+            sAlert.success('Upload complete!', undefined); // https://atmospherejs.com/juliancwirko/s-alert
+
+          }
+        });
+      }
+    });
+  }
+});
+//new code starts here, in case you need to work from a place from where it works
+Template.candidateOutput.helpers({
+  candidate:function(){
+    return Candidates.find();
+  }
 });
 
--- server/main.js
+++ server/main.js
@@ -4,3 +4,37 @@ Meteor.startup(() => {
   // code to run on server at startup
 });
 
+
+Meteor.methods({
+  parseUpload( data ) {
+    check( data, Array );
+
+    for ( let i = 0; i < data.length; i++ ) {
+      let item   = data[ i ],
+          firstNameLookupKey = "First Name",
+          firstNameLookupValue = item["First Name"],
+          lastNameLookupKey = "Last Name",
+          lastNameLookupValue = item["Last Name"],
+          exists = Candidates.findOne( { firstNameLookupKey: firstNameLookupValue, lastNameLookupKey: lastNameLookupValue  } );
+
+        //console.log("item, key, val, exists are: ", item, lookupKey, lookupValue, exists);
+
+      // If this row has not been inserted, and it has a firstname and lastName
+      if ( !exists && firstNameLookupValue && lastNameLookupValue) {
+        Candidates.insert( item );
+      } else {
+        console.warn( 'Rejected. This item already exists.' );
+      }
+    }
+  }
+});
+
+//Houston.add_collection(Sites);
+
+/*Houston.methods("Candidates", {
+  "candidateInfo_fromCSV": function (post) {
+    Candidates.find(post._id, {$set: {published: true}});
+    return post.name + " published successfully.";
+  }
+}); */
+/*lines 25-30, added this method to publish Candidates collection on the houston admin. still not sure exactly that this did. after commenting these lines out, reseting meteor, uploading the csv again and then opening the houston admin, the "candidateInfo_fromCSV" collection displays--but only the first row. none of the values from rows after the first ones are displaying. just a note, but not sure if this was even on the right path. *bél* */
