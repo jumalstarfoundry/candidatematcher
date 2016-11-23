85789494f023ad32fd4914c45b0eaeb8084d145a
learningdesignlabs <jumal@learningdesignlabs>
2016-11-22 19:52:18 -0800

    upgrade match feature

--- client/js/manageCandidates.js
+++ client/js/manageCandidates.js
@@ -1,5 +1,6 @@
 import { Template } from 'meteor/templating';
 import './../views/manageSites.html';
+import _ from 'underscore';
 
 /*
 getRandomInt = function(min, max) {
@@ -27,33 +28,64 @@ findShift = function(doc){
 
 var canBePlaced = function(candidate, site) {
 
+    var businessRulesMet = {};
+
+    // Reject candidate if cannot work 2 days or more
+    const minimumNumberOfDays = 2;
+    businessRulesMet.willingToWorkEnough = candidate["Number of Days to Work"] >= minimumNumberOfDays;
+
+console.log("testing whether candidate can work enough...");
+    if (!businessRulesMet.willingToWorkEnough) return false;
+
+console.log("work enough test passed...");
+
+    // We've determined that the candidate is willing to work enough, but we still need to see
+    // if the candidate is actually available during which site operation hours:
+
     // For each day, compare candidate start and end times to site start and end times
     var daysAvailableAtSite = 0;
-    var candidateStartTime, candidateEndTime;
     var siteStartTime, siteEndTime;
 
     // special cases to deal with
     var candidateFreeFriday = false;
     var candidateFreeEarlyWedPlusOneDay = false;
 
-    candidateStartTime = candidate[day.charAt(0).toUpperCase() + day.substr(1) + " Start Time"];
-    candidateEndTime = candidate[day.charAt(0).toUpperCase() + day.substr(1) + " End Time"];
-    siteStartTime = site[day.charAt(0).toLowerCase() + day.substr(1) + "Start"];
-    siteEndTime = site[day.charAt(0).toLowerCase() + day.substr(1) + "End"];
+
+    var findCandidateStartTime = function(day){
+      return candidate[day.charAt(0).toUpperCase() + day.substr(1) + " Start Time"];
+    }
+
+    var findCandidateEndTime = function(day){
+      return candidate[day.charAt(0).toUpperCase() + day.substr(1) + " End Time"];
+    }
+
+    var findSiteStartTime = function(day){
+      return site[day.charAt(0).toLowerCase() + day.substr(1) + "Start"];
+    }
+
+    var findSiteEndTime = function(day){
+      return site[day.charAt(0).toLowerCase() + day.substr(1) + "End"];
+    }
 
     var candidateMatchesWithSite = function(day)
       {
-    
-        const minimumMillsecondsToWork = 3600000; // 1 hour
+        var candidateStartTime, candidateEndTime;
+
+        candidateStartTime = findCandidateStartTime(day);
+        candidateEndTime = findCandidateEndTime(day);
+        siteStartTime = findSiteStartTime(day);
+        siteEndTime = findSiteEndTime(day);
+
+        ///const minimumMillsecondsToWork = 3600000; // 1 hour // removed minimum work time feature, since candidate must be able to work for the entirety of the shift
 
         var candidateCanShowUpBeforeClosing = moment(siteEndTime, "HH:mm").diff(moment(candidateStartTime, "HH:mm")) >= 0;
         var candidateCanLeaveAfterOpening =  moment(candidateEndTime, "HH:mm").diff(moment(siteStartTime, "HH:mm")) >= 0;
-        var timeAvailableForWork = moment(candidateEndTime, "HH:mm").diff(moment(candidateStartTime, "HH:mm"));
+        ///var timeAvailableForWork = moment(candidateEndTime, "HH:mm").diff(moment(candidateStartTime, "HH:mm"));
 
         console.log(candidate["First Name"] + " " + candidate["Last Name"] + " can show up before closing: ", candidateCanShowUpBeforeClosing);
         console.log(candidate["First Name"] + " " + candidate["Last Name"] + " can leave after opening: ", candidateCanLeaveAfterOpening);
         
-        return candidateCanShowUpBeforeClosing && candidateCanLeaveAfterOpening && (timeAvailableForWork >= minimumMillsecondsToWork);
+        return candidateCanShowUpBeforeClosing && candidateCanLeaveAfterOpening; // && (timeAvailableForWork >= minimumMillsecondsToWork);
 
       }
 
@@ -71,7 +103,7 @@ var canBePlaced = function(candidate, site) {
       {
         daysAvailableAtSite++;
 
-        candidateFreeEarlyWedPlusOneDay = moment("14:00", "HH:mm").diff(moment(candidateStartTime, "HH:mm")) <= 0;
+        candidateFreeEarlyWedPlusOneDay = moment("14:00", "HH:mm").diff(moment(findCandidateStartTime("Wednesday"), "HH:mm")) <= 0;
       }  
 
       if(candidateMatchesWithSite("Thursday"))
@@ -88,19 +120,12 @@ var canBePlaced = function(candidate, site) {
       console.log("available number of days is ", daysAvailableAtSite);
 
 
-var businessRulesMet = {};
-
-// Run candidate specific rules
-const minimumNumberOfDays = 2;
-businessRulesMet.daysAvailable = daysAvailableAtSite >= minimumNumberOfDays;
-
-// Run site vs. candidate rules
+businessRulesMet.schedulesMatch = daysAvailableAtSite >= minimumNumberOfDays; 
 businessRulesMet.travelWillingness = site.requiresTravel == candidate['Willing to Travel?'];
-businessRulesMet.freeFridayPlusOneDay = site.requiresFreeFridayPlusOneDay ? candidateFreeFriday : true;
-businessRulesMet.freeOnEarlyWedPlusOneDay = site.requiresFreeEarlyWedPlusOneDay ? candidateFreeEarlyWedPlusOneDay : true;
 
-return businessRulesMet.daysAvailable
-    && businessRulesMet.travelWillingness;
+
+return businessRulesMet.travelWillingness && businessRulesMet.schedulesMatch;
+   
 
 };
 
@@ -125,11 +150,17 @@ Template.manageCandidates.events({
 */
 
 
+// STEP 1: REJECT ALL FOLKS WHO CAN ONLY WORK FEWER THAN 2 DAYS
+// STEP 2: RUN ALGORITHM ON WORK STUDY OR UNITS
+// STEP 3: RUN ALGORITHM ON VOLUNTEERS FOR REMAINING SITES 
 
 
+
+// STEP 2: 
    var unassignedCandidates = Candidates.find({prioritySite:{ $exists: false}}); // toggle switch
    var availableSites = Sites.find();
    var potentialAssignments = {};
+   var candidatePriorityTracker = [];
 
 
     sAlert.success('Auto-match starting... !', undefined); // https://atmospherejs.com/juliancwirko/s-alert
@@ -144,7 +175,11 @@ Template.manageCandidates.events({
 //                                                      var candId = candidate._id;
                                                       
                                                       // Take note of the fact that this candidate can be assigned to this site
+                                                      
+                                                      // If the site has already received assignments, there is no need to 
+                                                      // initialize an object at the site key value
                                                       //
+
                                                       if(potentialAssignments[site.name])
                                                         {
                                                           potentialAssignments[site.name][candidate._id] = candidate;
@@ -157,6 +192,22 @@ Template.manageCandidates.events({
                                                         //_.extend(potentialAssignments[site.name], {candId:candidate});//_.extend(potentialAssignments[site.name],  {candidate["_id"]:candidate}  );
                                                       }
 
+                                                      // Create a separate array with objects, to have a list we can sort (later) and use to know which candidate to place first
+                                                      // It should look something like:
+                                                      // [ {candIDwerwer: 3 (days available)}, {candIDklasjlk: 3 }, {candIDasjejk3: 2}, ... ]
+                                                      // Using this array helps us avoid having to deal with merging the duplicates from the placementMatrix
+                                                      //
+                                                      // Only add an entry in this array if the candidate hasn't been added yet
+                                                      console.log("Testing if candidatePriorityTracker has the candidate id...", candidatePriorityTracker, "findIndex", _.findIndex(candidatePriorityTracker, {id: candidate._id}));
+
+                                                      if ( _.findIndex(candidatePriorityTracker, {id: candidate._id}) == -1 )
+                                                      {
+                                                        console.log("nope, no id is currently being tracked.....");
+                                                      
+                                                        candidatePriorityTracker.push({id: candidate._id, daysWillingToWork: candidate["Number of Days to Work"], workStatus: candidate["Preferred Volunteer Status"]});
+                                                      }
+
+
                                                       candidatePlacementCount++;
                                                   }
                                                 }
@@ -165,27 +216,59 @@ Template.manageCandidates.events({
         // DEBUG
         //
         console.log("number of sites candidate can be placed is " + candidatePlacementCount);
+        console.log("However, candidate has requested to only work", candidate["Number of Days to Work"]);
 
 
+        /*
+        ///// Remove matchPriority, no longer needed
         // If this candidate can be placed somewhere, then we can assign them a match priority. 
         //
         if(candidatePlacementCount >= 1){
+      
+
           Candidates.update(candidate._id, {$set: { matchPriority: 1/candidatePlacementCount*1000+1000 }});
           }
+
         // Otherwise, there are no sites we can find for them, so we mark them as unmactched
         //
         else
         {
           Candidates.update(candidate._id, {$set: { matchPriority: 999999999 }});
         }
+
+        */
     });
  
     // Now we should have a placement matrix 
 
-    console.log("The placement matrix we've found is: ", potentialAssignments)
+    console.log("The placement matrix we've found is: " , potentialAssignments);
+
+    console.log("The ranking of days to work is " , candidatePriorityTracker);
     // CALCULATE AND STORE SITE RATIOS FOR POTENTIAL PLACEMENTS FOR FINAL PLACEMENT
 
+    // Calculate the number of openings at each site
+
+
+    // Go through the potentialAssignments and start applying rules
+    // 
+
+// Run site vs. candidate rules
+var candidateSiteFit = function(candidate, site){
+  var businessPreferencesMet = {};
+
+  //
+  businessPreferencesMet.freeFridayPlusOneDay = site.prefersFreeFridayPlusOneDay ? candidateFreeFriday : true;
+  businessPreferencesMet.freeOnEarlyWedPlusOneDay = site.prefersFreeEarlyWedPlusOneDay ? candidateFreeEarlyWedPlusOneDay : true;
+  businessPreferencesMet.speaksSpanish = site.prefersSpanish
+
+
+
+}
+
+// NEXT: MODIFY SITE SCHEMA TO REFLECT SITE BIZ RULES
   
+
+
     sAlert.success('Auto-match complete!', undefined); // https://atmospherejs.com/juliancwirko/s-alert
 
 
